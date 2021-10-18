ad2c432f00194c3f6a3014f2cd7e2214b9d2fc08
FedeFluork <58691996+FedeFluork@users.noreply.github.com>
2021-10-18 15:17:21 +0200

    Updated behaviour of Main Display Board

--- client/src/components/MyMonitor.js
+++ client/src/components/MyMonitor.js
@@ -7,26 +7,32 @@ function MyMonitor() {
   const [queues, setQueues] = useState();
   const [tickets, setTickets] = useState();
 
-  useEffect(() => {
-    API.loadQueues().then((q) => { if (q.error === undefined) setQueues(q); });
+  const updateMonitor = () => {
+    API.loadQueues().then((q) => { if (q.error === undefined)  setQueues(q); });
     API.loadServings().then((t) => { if (t.error === undefined) setTickets(t); });
+  };
+
+  useEffect(() => {
+    updateMonitor();
+    setInterval(() => updateMonitor(), 2000);
   }, []);
 
   return (
     <Container className="bg-dark min-height-100 justify-content-center align-items-center text-center m-0 p-0" fluid>
       <h1 className="text-white pt-5 pb-1 pr-0 pl-4">Main Display Board</h1>
       <h5 className="text-white pt-0 pb-5 pr-0 pl-4">The number in green is the one being served.</h5>
-      {queues &&
+      {queues ?
         Object.keys(queues).map((c) =>
           <ListGroup horizontal="md" className="mt-3 ml-5 align-items-center" key={c}>
             <ListGroup.Item variant="primary"><h2>{c}</h2></ListGroup.Item>
             <ListGroup.Item variant="success">
-              <h6>{tickets && tickets[c] ? tickets[c].counter : " "}</h6>
+              <h6>{tickets && tickets[c] ? 'Counter ' + tickets[c].counter : " "}</h6>
               <h3>{tickets && tickets[c] ? tickets[c].ticket : " "}</h3>
             </ListGroup.Item>
             {queues[c].map(i => <ListGroup.Item key={i}><h3>{i}</h3></ListGroup.Item>)}
           </ListGroup>
-        )
+        ) :
+        <h1 className="text-white pt-5 pb-1 pr-0 pl-4">There are no tickets in queue at the moment.</h1>
       }
     </Container>
   );
